5debf4a94e63d7fab1e09178e8cfa9d3eac4ebbe
Ideve Core <ideve.core@gmail.com>
2024-06-01 18:32:37 -0300

    Update timer-controls component

--- src/components/timer-controls/main.js
+++ src/components/timer-controls/main.js
@@ -1,4 +1,4 @@
-/* timer-controls.js
+/* main.js
  *
  * Copyright 2023 Ideve Core
  *
@@ -16,8 +16,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
+ *
  */
 
+/**
+ *
+ * @class TimerControlsScope
+ * @extends GObject.Object
+ * @implements {Gtk.BuilderScope}
+ *
+ */
 import GObject from 'gi://GObject';
 import Gtk from 'gi://Gtk';
 import GLib from 'gi://GLib';
@@ -29,6 +37,17 @@ const TimerControlsScope = GObject.registerClass(
     Implements: [Gtk.BuilderScope],
   },
   class TimerControlsScope extends GObject.Object {
+    /**
+     *
+     * Creates a closure for a template signal handler.
+     *
+     * @param {Gtk.Builder} builder The builder object.
+     * @param {string} handlerName The name of the template signal handler.
+     * @param {Gtk.BuilderClosureFlags} flags Flags for the closure.
+     * @param {GObject} connectObject The object to connect the signal to.
+     * @returns {Function} The closure for the template signal handler.
+     *
+     */
     vfunc_create_closure(builder, handlerName, flags, connectObject) {
       if (flags & Gtk.BuilderClosureFlags.SWAPPED)
         throw new Error('Unsupported template signal flag "swapped"');
@@ -38,11 +57,27 @@ const TimerControlsScope = GObject.registerClass(
 
       return this[handlerName].bind(connectObject || this);
     }
+    /**
+     *
+     * Creates a new TimerControlsScope instance.
+     *
+     * @param {object} options
+     * @param {Adw.Application} options.application The application object.
+     * @param {object} options.timer The timer object.
+     *
+     */
     constructor({ application, timer }) {
       super();
       this._timer = timer;
       this._application = application;
     }
+    /**
+     *
+     * Starts or pauses the timer.
+     *
+     * If the timer is stopped, starts it. Otherwise, pauses it.
+     *
+     */
     _on_start_pause_timer() {
       if (this._timer.technique.get_data().timer_state === 'stopped') {
         start_timer({ application: this._application }).present(this._application.get_active_window());
@@ -50,12 +85,27 @@ const TimerControlsScope = GObject.registerClass(
         this._timer.technique.start();
       }
     }
+    /**
+     *
+     * Skips the timer to the next interval.
+     *
+     */
     _skip_timer() {
       this._timer.technique.skip();
     }
+    /**
+     *
+     * Resets the timer to its initial state.
+     *
+     */
     _reset_timer() {
       this._timer.technique.reset();
     }
+    /**
+     *
+     * Stops the timer.
+     *
+     */
     _stop_timer() {
       this._timer.technique.stop();
     }
@@ -64,32 +114,31 @@ const TimerControlsScope = GObject.registerClass(
 
 /**
  *
- * Create timer controls
+ * Creates and returns a timer controls component.
  *
- * @param {Object} params
- * @param {Adw.Application} params.application
+ * @param {GObject.Application} application The application object.
+ * @returns {Gtk.Widget} The timer controls component.
  *
  */
 export const timer_controls = ({ application }) => {
   const builder = new Gtk.Builder();
-  const timer = application.utils.timer;
 
-  builder.set_scope(new TimerControlsScope({ application, timer }));
+  builder.set_scope(new TimerControlsScope({ application, timer: application.utils.timer }));
   builder.add_from_resource(Resource);
 
   const pomodoro_item = application.utils.pomodoro_item;
   const component = builder.get_object("component");
 
-  timer.connect('start', () => {
+  application.utils.timer.connect('start', () => {
     component.visible_child_name = 'running_timer';
   });
-  timer.connect('pause', () => {
+  application.utils.timer.connect('pause', () => {
     component.visible_child_name = 'paused_timer';
   });
-  timer.connect('end', () => {
+  application.utils.timer.connect('end', () => {
     component.visible_child_name = 'paused_timer';
   });
-  timer.connect('stop', () => {
+  application.utils.timer.connect('stop', () => {
     component.visible_child_name = 'init_timer';
   });
 
