0a6851cfebdc33e39dd7a7a6cc2a3eae835bb59a
Ideve Core <ideve.core@gmail.com>
2024-06-01 21:17:16 -0300

    Update main window

--- src/window.js
+++ src/window.js
@@ -21,14 +21,79 @@
 import GObject from 'gi://GObject';
 import Adw from 'gi://Adw';
 import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
 import GLib from 'gi://GLib';
+import { shortcuts } from './components/shortcuts/main.js';
 import { timer } from './pages/timer/main.js';
-import { Statistics } from './pages/statistics/statistics.js';
-import { ThemeSelector } from './components/theme-selector/theme-selector.js';
-import Shortcuts from './components/shortcuts/shortcuts.js';
-import { SmallWindow } from './components/small-window/small-window.js';
+import { statistics } from './pages/statistics/main.js';
 import { display_timer } from './components/display-timer/main.js';
-import Template from './window.blp' assert { type: 'uri' };
+import Resource from './window.blp';
+
+/**
+ *
+ * Creates and returns a timer controls component.
+ *
+ * @param {GObject.Application} application The application object.
+ * @returns {Gtk.Widget} The timer controls component.
+ *
+ */
+export const window = ({ application }) => {
+  const builder = new Gtk.Builder();
+  const settings = application.utils.settings;
+
+  builder.add_from_resource(Resource);
+
+  const component = builder.get_object("component");
+  const shorten_window_button = builder.get_object("shorten_window_button");
+
+  settings.bind(
+    "width",
+    component,
+    "default-width",
+    Gio.SettingsBindFlags.DEFAULT,
+  );
+  settings.bind(
+    "height",
+    component,
+    "default-height",
+    Gio.SettingsBindFlags.DEFAULT,
+  );
+  settings.bind(
+    "is-maximized",
+    component,
+    "maximized",
+    Gio.SettingsBindFlags.DEFAULT,
+  );
+  settings.bind(
+    "is-fullscreen",
+    component,
+    "fullscreened",
+    Gio.SettingsBindFlags.DEFAULT,
+  );
+
+  builder.get_object("timer_page").set_child(
+    timer(
+      {
+        application,
+        display_timer: display_timer({ application }),
+      }
+    )
+  );
+  builder.get_object("statistics_page").set_child(
+    statistics({ application })
+  );
+
+  application.utils.timer.connect('start', () => shorten_window_button.set_visible(true));
+  application.utils.timer.connect('stop', () => shorten_window_button.set_visible(false));
+
+  component.set_help_overlay(shortcuts({ application }));
+  component.set_application(application);
+  component.connect('close-request', () => {
+    application.quit_request();
+    return true;
+  });
+  return component;
+}
 
 /**
  *
@@ -37,82 +102,47 @@ import Template from './window.blp' assert { type: 'uri' };
  * @extends {Adw.ApplicationWindow}
  *
  */
-export class Window extends Adw.ApplicationWindow {
-  static {
-    GObject.registerClass({
-      Template,
-      GTypeName: 'Window',
-      InternalChildren: [
-        'stack',
-        'shorten_window',
-        'menu_button',
-        'toast_overlay',
-        'statistics_page',
-        'timer_page',
-      ],
-    }, this);
-  }
+// export class Window extends Adw.ApplicationWindow {
+//   static {
+//     GObject.registerClass({
+//       Template,
+//       GTypeName: 'Window',
+//       InternalChildren: [
+//         'stack',
+//         'shorten_window',
+//         'menu_button',
+//         'toast_overlay',
+//         'statistics_page',
+//         'timer_page',
+//       ],
+//     }, this);
+//   }
 
-  /**
-   *
-   * Create a instance of pomodoro main window
-   * @param {Adw.ApplicationWindow} application
-   *
-   */
-  constructor(application) {
-    super({ application });
+//     this._setup_actions();
+//     this._small_window.insert_action_group("window", this.window_group);
+//     this._shorten_window.set_sensitive(false);
+//   }
 
-    const theme_selector = new ThemeSelector({ application });
-    const display_timer_component = display_timer({ application });
-    this._menu_button.get_popover().add_child(theme_selector, 'theme');
-    this.set_help_overlay(new Shortcuts(application));
-    this._timer_page.set_child(timer({ application, display_timer: display_timer_component }));
-    this._statistics_page_component = new Statistics({ application });
-    this._statistics_page.set_child(this._statistics_page_component);
-    this._small_window = new SmallWindow({ application, display_timer_component });
-    this._timer = application.utils.timer;
+//   /**
+//    *
+//    * Setup actions
+//    *
+//    */
+//   _setup_actions() {
+//     const toggle_small_window = new Gio.SimpleAction({ name: 'toggle-small-window', parameter_type: new GLib.Variant('s', '').get_type() });
+//     this.window_group = new Gio.SimpleActionGroup();
 
-    this._timer.connect('start', () => {
-      this._shorten_window.set_sensitive(true);
-    });
-    this._timer.connect('stop', () => {
-      this._shorten_window.set_sensitive(false);
-    });
-    this.connect('close-request', () => {
-      application.quit_request();
-      return true;
-    });
-    this._stack.connect('notify::visible-child', () => {
-      if (this._stack.visible_child_name == 'statistics') {
-        this._statistics_page_component.load_statistics_data();
-      }
-    });
-
-    this._setup_actions();
-    this._small_window.insert_action_group("window", this.window_group);
-    this._shorten_window.set_sensitive(false);
-  }
-
-  /**
-   *
-   * Setup actions
-   *
-   */
-  _setup_actions() {
-    const toggle_small_window = new Gio.SimpleAction({ name: 'toggle-small-window', parameter_type: new GLib.Variant('s', '').get_type() });
-    this.window_group = new Gio.SimpleActionGroup();
-
-    toggle_small_window.connect('activate', (simple_action, parameter) => {
-      const value = parameter.get_string()[0];
-      if (value === 'open') {
-        this.hide();
-        this._small_window.present();
-      } else {
-        this.present();
-        this._small_window.hide();
-      }
-    });
-    this.add_action(toggle_small_window);
-    this.window_group.add_action(toggle_small_window)
-  }
-}
+//     toggle_small_window.connect('activate', (simple_action, parameter) => {
+//       const value = parameter.get_string()[0];
+//       if (value === 'open') {
+//         this.hide();
+//         this._small_window.present();
+//       } else {
+//         this.present();
+//         this._small_window.hide();
+//       }
+//     });
+//     this.add_action(toggle_small_window);
+//     this.window_group.add_action(toggle_small_window)
+//   }
+// }
